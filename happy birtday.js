c257bc2242f456096a2409c7bc2ca0bf6e713045
faisal1705 <pathanfaisalkhan20@gmail.com>
2025-02-18 01:02:45 +0530

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "To You "],
+    strings: ["HAPPY", "BIRTHDAY!", "To You HONEY"],
     charSize: 20,
     charSpacing: 20,
     lineHeight: 30,
